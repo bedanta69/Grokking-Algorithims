bd529e5c0ec2cbb4eeb3d9d01c75695f5019fc51
Bedanta Kashyap <116745791+bedanta69@users.noreply.github.com>
2025-02-18 19:07:06 +0530

    Add files via upload

---
+++ Selection sort/Binarysearch.py
@@ -0,0 +1,16 @@
+def binary_search(list,items):
+    low = 0
+    high =len(list)-1
+    while low <= high:
+        mid = low + high //2
+        guess = list[mid]
+        if(guess == item):
+            return mid
+            break;
+        elif(guess>mid):
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return NUll
+#the runtime for binary search is O(log n)
